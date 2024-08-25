5ab77af6c37087830a66611dab72d69273b46373
AndyOoh <andy.oeee@gmail.com>
2024-08-25 19:17:45 +0700

    uncomment pragma in unused teste files

--- test/AbstractYodlRouter/AbstractYodlRouter.emitConversionEvent.t.sol
+++ test/AbstractYodlRouter/AbstractYodlRouter.emitConversionEvent.t.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-// pragma solidity ^0.8.26;
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
 
 // import "forge-std/Test.sol";
 // import {AbstractYodlRouter} from "../../src/AbstractYodlRouter.sol";
