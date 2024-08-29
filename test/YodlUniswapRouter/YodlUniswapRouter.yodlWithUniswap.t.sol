9306bfcdb1d3af189901c694e52b7cba6240aa25
AndyOoh <andy.oeee@gmail.com>
2024-08-29 18:55:45 +0700

    YodlUniswapRouterHarness

---
+++ test/YodlUniswapRouter/YodlUniswapRouter.yodlWithUniswap.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import {AbstractYodlRouter} from "../../src/AbstractYodlRouter.sol";
+import {ISwapRouter02} from "../../src/routers/YodlUniswapRouter.sol";
+import {AbstractYodlRouterHarness} from "./shared/AbstractYodlRouterHarness.t.sol";
+
+contract YodlUniswapRouterTest is Test {
+    AbstractYodlRouterHarness abstractRouter;
+    uint256 constant APPROX_MAX_AMOUNT = 1e68;
+    // address mockWrappedNativeToken;
+    // address mockYodlFeeTreasury;
+    // uint256 constant YODL_FEE_BPS = 100; // 1%
+
+    function setUp() public {
+        // mockWrappedNativeToken = address(0x1);
+        // mockYodlFeeTreasury = address(0x2);
+
+        // helperContract = new AbstractYodlRouterHarness(mockWrappedNativeToken, mockYodlFeeTreasury, YODL_FEE_BPS);
+        abstractRouter = new AbstractYodlRouterHarness();
+    }
+
+    /* 
+    * Scenario: fuzz testing amount and feeBps
+    */
+    function testFuzz_CalculateFee(uint256 amount, uint256 feeBps) public view {}
+}
