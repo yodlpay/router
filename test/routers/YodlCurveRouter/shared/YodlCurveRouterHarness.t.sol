d60904dc85f9e9a1d60396655d0e9b1057e7f8a8
AndyOoh <andy.oeee@gmail.com>
2024-09-10 00:40:56 +0700

    router harnesses - set params

--- test/routers/YodlCurveRouter/shared/YodlCurveRouterHarness.t.sol
+++ test/routers/YodlCurveRouter/shared/YodlCurveRouterHarness.t.sol
@@ -8,7 +8,10 @@ import {YodlCurveRouter} from "@src/routers/YodlCurveRouter.sol";
 import {AbstractYodlRouterHarness} from "@test/AbstractYodlRouter/shared/AbstractYodlRouterHarness.t.sol";
 
 contract YodlCurveRouterHarness is YodlCurveRouter, AbstractYodlRouterHarness {
-    constructor(address _curveRouter) YodlCurveRouter(_curveRouter) AbstractYodlRouterHarness() {}
+    constructor(AbstractYodlRouter.ChainType _chainType, address _sequencerUptimeFeed, address _curveRouter)
+        YodlCurveRouter(_curveRouter)
+        AbstractYodlRouterHarness(_chainType, _sequencerUptimeFeed)
+    {}
 
     /* Override verifyRateSignature to resolve diamond inheritance */
 
