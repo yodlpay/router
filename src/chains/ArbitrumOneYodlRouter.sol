fd6f01e05a6876d5452eb394adcad24bd24646dd
Andy <andy.oeee@gmail.com>
2024-11-26 12:07:35 +0700

    add across to optimism, arbitrum, polygon

--- src/chains/ArbitrumOneYodlRouter.sol
+++ src/chains/ArbitrumOneYodlRouter.sol
@@ -6,11 +6,13 @@ pragma solidity ^0.8.26;
 import "../routers/YodlTransferRouter.sol";
 import "../routers/YodlCurveRouter.sol";
 import "../routers/YodlUniswapRouter.sol";
+import "../routers/YodlAcrossRouter.sol";
 
-contract YodlRouter is YodlTransferRouter, YodlCurveRouter, YodlUniswapRouter {
+contract YodlRouter is YodlTransferRouter, YodlAcrossRouter, YodlCurveRouter, YodlUniswapRouter {
     constructor()
         AbstractYodlRouter()
         YodlTransferRouter()
+        YodlAcrossRouter(0xe35e9842fceaCA96570B734083f4a58e8F7C5f2A)
         YodlCurveRouter(0x2191718CD32d02B8E60BAdFFeA33E4B5DD9A0A0D)
         YodlUniswapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45)
     {
@@ -20,3 +22,4 @@ contract YodlRouter is YodlTransferRouter, YodlCurveRouter, YodlUniswapRouter {
         wrappedNativeToken = IWETH9(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
     }
 }
+
