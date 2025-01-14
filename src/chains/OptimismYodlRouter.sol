b6fe1f60a4883d76073c06324bc60c299a661cd3
yodlmeister <104564244+yodlmeister@users.noreply.github.com>
2025-01-14 13:30:52 +0700

    payWithAcross (#18)
    * scaffold payWithAcross
    
    * add DEFAULT_ANVIL_KEY
    
    * fix exclusivityDeadline. add deploy-anvil in make
    
    * exclusivityDeadline overflow check
    
    * wip
    
    * format
    
    * add yodl-across identifier to depositV3 callData
    
    * Add Across to more chains (#17)
    
    * add across to optimism, arbitrum, polygon
    
    * format
    
    ---------
    
    Co-authored-by: Andy <andy.oeee@gmail.com>

--- src/chains/OptimismYodlRouter.sol
+++ src/chains/OptimismYodlRouter.sol
@@ -6,11 +6,13 @@ pragma solidity ^0.8.26;
 import "../routers/YodlTransferRouter.sol";
 import "../routers/YodlCurveRouter.sol";
 import "../routers/YodlUniswapRouter.sol";
+import "../routers/YodlAcrossRouter.sol";
 
-contract YodlRouter is YodlTransferRouter, YodlCurveRouter, YodlUniswapRouter {
+contract YodlRouter is YodlTransferRouter, YodlAcrossRouter, YodlCurveRouter, YodlUniswapRouter {
     constructor()
         AbstractYodlRouter()
         YodlTransferRouter()
+        YodlAcrossRouter(0x6f26Bf09B1C792e3228e5467807a900A503c0281)
         YodlCurveRouter(0x0DCDED3545D565bA3B19E683431381007245d983)
         YodlUniswapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45)
     {
